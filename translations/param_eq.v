63722fd4e28725b0fa3f8799dc326cbff7b3cbb7
Marcel Ullrich <ullrich.marcel@web.de>
2020-12-02 11:25:18 +0100

    applyEnv lifting connection proof

--- translations/param_eq.v
+++ translations/param_eq.v
@@ -1,6 +1,82 @@
 
 Load param_unary.
 
+Definition applyEnv := tsl_rec0_2.
+
+Lemma envUpLift x n k (E:Env): EnvUp (EnvLift E n k) x = EnvLift (EnvUp E) n (S k) x.
+Proof.
+    destruct x;intros;[easy|].
+    unfold EnvLift; cbn.
+    destruct leb;lia.
+Qed.
+
+Lemma envUpEq E E': 
+    (forall x, E x = E' x) ->
+    forall y, EnvUp E y = EnvUp E' y.
+Proof.
+    intros H [];cbn;trivial.
+    now rewrite H.
+Qed.
+(* Lemma envUpEq E E' t: 
+applyEnv E t = applyEnv E' t ->
+applyEnv (EnvUp E) t = applyEnv (EnvUp E') t.
+Proof.
+    intro H;induction t using term_forall_list_ind;cbn;eauto.
+    - destruct n;cbn;[trivial|].
+    cbn in H. *)
+
+Lemma applyEnvCongr E E' t: (forall x, E x = E' x) -> applyEnv E t = applyEnv E' t.
+Proof.
+    intros H;induction t using term_forall_list_ind in E, E', H |- *;cbn;eauto.
+    (* all: try rewrite IHt;
+        try rewrite IHt1;
+        try rewrite IHt2;eauto. *)
+    - f_equal. induction H0;trivial;cbn.
+      f_equal;[now apply H0|]. apply IHForall.
+     (* now rewrite H0, IHForall. *)
+    - f_equal;[apply IHt1|apply IHt2];assumption.
+    - f_equal;[now apply IHt1|].
+      now apply IHt2, envUpEq.
+    - f_equal;[now apply IHt1|].
+      now apply IHt2, envUpEq.
+    - f_equal;[now apply IHt1|now apply IHt2|].
+      now apply IHt3, envUpEq.
+    - f_equal. 
+      1: now apply IHt.
+      induction H0;trivial;cbn.
+      f_equal;[now apply H0|]. apply IHForall.
+    - f_equal;[apply IHt1|apply IHt2|];try assumption.
+      induction X;trivial;cbn.
+      f_equal;trivial. destruct x;now f_equal.
+    - f_equal. now apply IHt.
+Qed.
+
+Goal forall t n k E, applyEnv (EnvLift E n k) t = lift n k (applyEnv E t).
+(* Goal forall t n k E, applyEnv (EnvLift E n k) t = (applyEnv E (lift n k t)). *)
+Proof.
+    intros t.
+    induction t using term_forall_list_ind;intros;eauto;cbn.
+    9-10: admit. (* fix *)
+    all: try rewrite IHt;
+        try rewrite IHt1;
+        try rewrite IHt2;eauto.
+    - f_equal. induction H;trivial;cbn.
+      now f_equal.
+    - f_equal. erewrite applyEnvCongr. 
+    2: intros x;now rewrite envUpLift.
+    now rewrite IHt2.
+    - f_equal. erewrite applyEnvCongr. 
+    2: intros x;now rewrite envUpLift.
+    now rewrite IHt2.
+    - f_equal. erewrite applyEnvCongr. 
+    2: intros x;now rewrite envUpLift.
+    now rewrite IHt3.
+    - f_equal. induction H;trivial;cbn.
+    now f_equal.
+    - f_equal. induction X;cbn;trivial.
+    destruct x;unfold on_snd;cbn;do 2 (f_equal;trivial).
+Admitted.
+
 (* lemmas about env vs lift *)
 
 (* Goal forall t E, tsl_rec1 E t = tsl_rec1_org E t.
